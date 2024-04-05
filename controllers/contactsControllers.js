9e195fc2be69cb0c078c08417a9723c49593068d
Pohrebniak Andrian <pogra29@gmail.com>
2024-04-06 01:40:09 +0300

    Done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,63 @@
-import contactsService from "../services/contactsServices.js";
+import  { getListContacts, getOneContactByid, deleteContactByid, addContact, updateContactById} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    try {
+        const list = await getListContacts();
+        res.json(list); 
+    } catch (error) {
+        console.log(error)
+    }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const contact = await getOneContactByid(id);
+        if (!contact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json(contact).status(200);
+    } catch (error) {
+        console.log(error)
+    }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact =async (req, res) => {
+    try {
+        const { id } = req.params;
+        const contact = await deleteContactByid(id);
+        if(!contact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json({
+            message: 'Contact deleted',
+            contact
+        }).status(200);
+    } catch (error) {
+        console.log(error)
+    }
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res) => {
+try {
+    const dataNewContact = req.body;
+    const newContact = await addContact(dataNewContact);
+    if(!newContact) {
+        return res.status(400).json({ message: 'Contact not created' });
+    }
+    res.json(newContact).status(201);
+} catch (error) {
+    console.log(error)
+} 
+};
+
+export const updateContact = async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+    const update = await updateContactById (id, data);
+    if(!update) {
+        return res.status(400).json({ message: 'Contact not updated' });
+    }
+    res.json(update).status(200);
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,13 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
+    name: Joi.string().required().min(3).max(30),
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com' , 'net'] } }),
+    phone: Joi.string().required().min(3).max(30),
 })
 
 export const updateContactSchema = Joi.object({
-
+    name: Joi.string(), 
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com' , 'net'] } }),
+    phone: Joi.string(),
 })
