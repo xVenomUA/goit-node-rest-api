68e06b8f035aef55a8ae74dacbcad8793b7ffad8
Pohrebniak Andrian <pogra29@gmail.com>
2024-04-23 23:53:00 +0300

    Done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -41,7 +41,6 @@ export const getCurrent = asyncCatch(async (req, res) => {
 
 export const updateAvatar = asyncCatch(async (req, res) => {
   if(req.file === undefined) throw new HttpError(400, "Field of avatar with file not found");
-  console.log(req)
     const user = await updateImage(req.body, req.user, req.file);
     res.status(200).json({avatarURL: user.avatarURL});
 }); 
